b87d778c0327e4463b06f65edb18269787565fd4
bago2k4 <iacopo@rc0.eu>
2021-03-29 18:24:12 +0200

    Add some debug logs, remove React.

--- index.js
+++ index.js
@@ -8,49 +8,61 @@ import { ContenteditableEditor } from "@textcomplete/contenteditable";
 import { TextareaEditor } from "@textcomplete/textarea";
 
 function emojiAutocompleteInner(editors) {
+  console.log(`DBG emojiAutocompleteInner ${editors}`);
   return editors.map(editor => {
-    var txtComp = new Textcomplete(editor, [{
-      match: /\B:([\-+\w]{1,30})$/,
-      search: function (term, callback) {
-        callback(emojiIndex.search(term));
-      },
-      template: function (emoji) {
-        return emoji.native +' '+emoji.colons;
-      },
-      replace: function (emoji) {
-        return emoji.native;
-      },
-      index: 1
-    }],
-    {dropdown: {
-      className: "emoji-autocomplete-menu textcomplete-dropdown dropdown-menu",
-      style: {zIndex: 1136},
-      maxCount: 20
+    console.log(`DBG setup editor ${editor}`);
+    let txtComp = new Textcomplete(editor, [{
+        match: /\B:([\-+\w]{1,30})$/,
+        search: (term, callback) => {console.log(`DBG textComplete.search with term ${term} and callback ${callback}`); callback(emojiIndex.search(term))},
+        template: emoji => {console.log(`DBG textComplete.template with emoji ${emoji} -> "${emoji.native} ${emoji.colons}"`); `${emoji.native} ${emoji.colons}`},
+        replace: emoji => {console.log(`DBG textComplete.replace with emoji ${emoji} -> ${emoji.native}`); emoji.native},
+        index: 1
+      }],
+      {dropdown: {
+       className: "emoji-autocomplete-menu textcomplete-dropdown dropdown-menu",
+       style: {zIndex: 1136},
+       maxCount: 20
     }});
-    txtComp.on("select", (e)=>{
-      let event = new Event("input", {bubbles: true});
-      editor.el.dispatchEvent(event);
+
+    txtComp.on("select", (inEvent) => {
+      console.debug(`TextComplete did select ${inEvent.target}`)
+      console.log(`DBG TextComplete.onSelect with event ${inEvent}`);
+      let outEvent = new Event("input", {bubbles: true});
+      console.log(`DBG    editor.el ${editor.el}`);
+      console.log(`DBG    ret ${outEvent}`);
+      editor.el.dispatchEvent(outEvent);
     });
     return txtComp;
   });
 }
 
 function setupEditorWithElement(el) {
+  console.log(`DBG setupEditorWithElement ${el}`);
   let editor;
-  if (el.getAttribute("contenteditable")) {
-    editor = new ContenteditableEditor(el);
-  }else if (el.nodeName.toLowerCase() === "textarea") {
-    editor = new TextareaEditor(el);
+  if (!el) {
+    console.error(`Can't initialize EmojiAutocopmlete on undefined element: ${el}`)
+    return editor;
   }
+  if (el.getAttribute("contenteditable")) {
+    console.debug(`Initializing EmojiAutocomplete on ${el.tagName} element: ${el}`)
+    editor = new ContenteditableEditor(el);
+  } else if (el.nodeName.toLowerCase() === "textarea") {
+    console.debug(`Initializing EmojiAutocomplete on TEXTAREA element: ${el}`)
+    editor = new TextareaEditor(el);
+  } else {
+    console.warn(`Element of type ${el.tagName} not supported`)
+  }
+  console.log(`DBG ret ${editor}`);
   return editor;
 }
 
 function emojiAutocomplete(elementOrSelector) {
+  console.log(`DBG emojiAutocomplete called with ${elementOrSelector}`);
   let textEditors = [];
   if (elementOrSelector.nodeType && elementOrSelector.nodeType === Node.ELEMENT_NODE) {
     textEditors.push(setupEditorWithElement(elementOrSelector));
   } else {
-    els = document.querySelectorAll(elementOrSelector);
+    const els = document.querySelectorAll(elementOrSelector);
     for (let i = 0; i < els.length; i++) {
       textEditors.push(setupEditorWithElement(els[i]));
     }
@@ -58,6 +70,7 @@ function emojiAutocomplete(elementOrSelector) {
   if (textEditors.length) {
     return emojiAutocompleteInner(textEditors);
   }
+  console.log(`DBG initialized emojiAutocomplete, textEditors: ${textEditors}`);
 }
 
 export default emojiAutocomplete;
